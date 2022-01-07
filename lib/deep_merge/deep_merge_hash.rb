a85c287c1cb2d65f0ad893e9f9df94036acd454e
Jason Frey <fryguy9@gmail.com>
2022-01-07 18:15:44 -0500

    Cleanup documentation
    - Update README examples with syntax highlighting
    - Add some more examples
    - Change options list to a table for readability
    - General Markdown cleanup
    - Fix code comments on certain methods

--- lib/deep_merge/deep_merge_hash.rb
+++ lib/deep_merge/deep_merge_hash.rb
@@ -2,7 +2,7 @@ require 'deep_merge/core'
 
 module DeepMerge
   module DeepMergeHash
-    # ko_hash_merge! will merge and knockout elements prefixed with DEFAULT_FIELD_KNOCKOUT_PREFIX
+    # ko_deep_merge! will merge and knockout elements prefixed with DEFAULT_FIELD_KNOCKOUT_PREFIX
     def ko_deep_merge!(source, options = {})
       default_opts = {:knockout_prefix => "--", :preserve_unmergeables => false}
       DeepMerge::deep_merge!(source, self, default_opts.merge(options))
--- lib/deep_merge/rails_compat.rb
+++ lib/deep_merge/rails_compat.rb
@@ -2,19 +2,19 @@ require 'deep_merge/core'
 
 module DeepMerge
   module RailsCompat
-    # ko_hash_merge! will merge and knockout elements prefixed with DEFAULT_FIELD_KNOCKOUT_PREFIX
+    # ko_deeper_merge! will merge and knockout elements prefixed with DEFAULT_FIELD_KNOCKOUT_PREFIX
     def ko_deeper_merge!(source, options = {})
       default_opts = {:knockout_prefix => "--", :preserve_unmergeables => false}
       DeepMerge::deep_merge!(source, self, default_opts.merge(options))
     end
 
-    # deep_merge! will merge and overwrite any unmergeables in destination hash
+    # deeper_merge! will merge and overwrite any unmergeables in destination hash
     def deeper_merge!(source, options = {})
       default_opts = {:preserve_unmergeables => false}
       DeepMerge::deep_merge!(source, self, default_opts.merge(options))
     end
 
-    # deep_merge will merge and skip any unmergeables in destination hash
+    # deeper_merge will merge and skip any unmergeables in destination hash
     def deeper_merge(source, options = {})
       default_opts = {:preserve_unmergeables => true}
       DeepMerge::deep_merge!(source, self, default_opts.merge(options))
